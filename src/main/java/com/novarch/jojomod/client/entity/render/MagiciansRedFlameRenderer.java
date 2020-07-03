b78a5b29934ca44586bf7b7aeeba96e25fe5bd9a
Novarch <ivan.pecotic25@gmail.com>
2020-07-03 19:39:24 +0200

    Switched to MatrixStack rendering instead of GL11

--- src/main/java/com/novarch/jojomod/client/entity/render/MagiciansRedFlameRenderer.java
+++ src/main/java/com/novarch/jojomod/client/entity/render/MagiciansRedFlameRenderer.java
@@ -5,12 +5,9 @@ import com.novarch.jojomod.JojoBizarreSurvival;
 import com.novarch.jojomod.client.entity.model.MagiciansRedFlameModel;
 import com.novarch.jojomod.entities.stands.AbstractStandPunchEntity;
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -34,21 +31,7 @@ public class MagiciansRedFlameRenderer extends StandPunchRenderer<AbstractStandP
 			flame.Flames.showModel = true;
 			flame.Flames2.showModel = false;
 		}
-		entityIn.rotationYaw = entityIn.standMaster.rotationYaw;
-		entityIn.rotationPitch = entityIn.standMaster.rotationPitch;
-		renderManager.textureManager.bindTexture(TEXTURE);
-		GL11.glPushMatrix();
-		GL11.glTranslatef((float) entityIn.getPosX(), (float) entityIn.getPosY(), (float) entityIn.getPosZ());
-		GL11.glRotatef(entityIn.prevRotationYaw + (entityIn.rotationYaw - entityIn.prevRotationYaw) * packedLightIn - 90.0F, 0.0F, 1.0F, 0.0F);
-		GL11.glRotatef(entityIn.prevRotationPitch + (entityIn.rotationPitch - entityIn.prevRotationPitch) * packedLightIn, 0.0F, 0.0F, 1.0F);
-		GL11.glEnable(32826);
-		Minecraft.getInstance().textureManager.bindTexture(TEXTURE);
-		GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F);
-		GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F);
-		GL11.glScalef(2.0F, 2.0F, 2.0F);
-		flame.render(matrixStackIn, bufferIn.getBuffer(RenderType.getEntitySmoothCutout(getEntityTexture(entityIn))), packedLightIn, 0);
-		GL11.glDisable(32826);
-		GL11.glPopMatrix();
+		super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn, flame);
 	}
 
 	@Nonnull
