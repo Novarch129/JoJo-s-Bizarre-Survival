bdd60fc460d2956a64062d4a5660b835f4b4fa17
Novarch <ivan.pecotic25@gmail.com>
2020-08-10 12:08:04 +0200

    Improvements to 20th Century Boy

--- src/main/java/io/github/novarch129/jojomod/entity/stand/TwentiethCenturyBoyEntity.java
+++ src/main/java/io/github/novarch129/jojomod/entity/stand/TwentiethCenturyBoyEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 @SuppressWarnings("ConstantConditions")
@@ -46,9 +45,9 @@ public class TwentiethCenturyBoyEntity extends AbstractStandEntity {
         super.tick();
         if (getMaster() != null) {
             Stand.getLazyOptional(master).ifPresent(props -> {
-                props.setAbilityActive(props.getTimeLeft() > 800 && props.getCooldown() <= 0 && props.getAbility());
+                props.setAbilityActive(props.getTimeLeft() > 801 && props.getCooldown() <= 0 && props.getAbility());
                 if (props.getAbilityActive()) {
-                    master.setMotion(Vec3d.ZERO);
+                    master.setMotion(0, master.onGround && !world.getBlockState(master.getPosition().down()).isAir(world, master.getPosition().down()) ? 0 : master.getMotion().getY(), 0);
                     props.subtractTimeLeft(1);
                 }
             });
